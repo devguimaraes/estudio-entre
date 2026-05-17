cd1935687b728ce31690df8a71867b56f00e8ee7
devguimaraes <devgmrs@gmail.com>
2026-05-17 20:44:07 -0300

    feat(sanity): adicionar queries GROQ para álbuns da galeria

--- src/sanity/queries/index.ts
+++ src/sanity/queries/index.ts
@@ -1,2 +1,3 @@
 export { eventosQuery, eventoBySlugQuery, todosEventosSlugsQuery } from "./evento";
 export { configuracaoQuery } from "./configuracao";
+export { albunsQuery, albumBySlugQuery, todosAlbunsSlugsQuery } from "./galeria";
